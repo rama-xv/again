90d518b689813c1a86df8345e3ff4cedca3abb78
Rama Ibseileh <ramaib@umich.edu>
2025-07-02 19:27:26 -0400

    Changed

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -1,6 +1,6 @@
 public class PrimeNumberChecker {
 public static void main(String[] args) {
-int number = 72; // Hardcoded input number
+int number = 62; // Hardcoded input number
 boolean isPrime = true;
 if (number <= 1) {
 isPrime = false;
